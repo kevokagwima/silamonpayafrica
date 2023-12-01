ecddaa6547905944c5cd0f37c8c5ac76a4d50b41
kevin <you@example.com>
2023-12-01 11:25:06 +0300

    Updated Files

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+const burger = document.querySelector(".burger");
+const side_nav = document.querySelector(".side-navigation");
+const close = document.getElementById("close");
+
+burger.addEventListener("click", () => {
+  side_nav.classList.add("show-side-nav");
+});
+
+close.addEventListener("click", () => {
+  side_nav.classList.remove("show-side-nav");
+});
+
+window.addEventListener("scroll", () => {
+  side_nav.classList.remove("show-side-nav");
+});
+
 var slideIndex = 1;
 showDivs(slideIndex);
 
